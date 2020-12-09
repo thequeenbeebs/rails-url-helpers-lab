b9f78eaa249c476b3339d3683b64cbcdc6f5285e
Blaire Baker <bakerblaire@gmail.com>
2020-12-08 18:10:54 -0600

    commit tree 6f8814751b425b29818e109bf1e0dd7127dc637a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,22 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def edit
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.update(active: true)
+    else
+      @student.update(active: false)
+    end
+    redirect_to student_path(@student)
+  end
+
   def show
+    if @student.active == false
+      @activity = "This student is currently inactive."
+    else
+      @activity = "This student is currently active."
+    end
   end
 
   private
