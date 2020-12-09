d85bc64748856bcaf9397fe483988855d4d4f7be
Blaire Baker <bakerblaire@gmail.com>
2020-12-08 18:11:01 -0600

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :students
+  get '/students/:id/edit', to: 'students#edit', as: 'activate_student'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
